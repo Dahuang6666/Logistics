795124c36910433a2832ef043eae23f67f956ab5
huang yan <1514055091@qq.com>
2025-12-22 10:33:11 +0800

    宿舍管理系统1.0

--- frontend/vue-project/src/utils/api.js
+++ frontend/vue-project/src/utils/api.js
@@ -11,9 +11,48 @@ export function login(data) {
 
 /**
  * 用户注册
- * @param data
+ * @param {Object} data
  * @returns {*}
  */
 export function register(data) {
   return request.post('/school/user/register', data);
+}
+
+/**
+ * 获取验证码
+ */
+export function getCaptcha() {
+  return request.get('/school/user/captcha');
+}
+
+/**
+ * 验证验证码
+ */
+
+export function verifyCaptcha(data) {
+  return request.post('/school/user/verifyCaptcha' , data);
+}
+
+/**
+ * 发送邮件验证码
+ * @param {string} userNo - 用户账号
+ */
+export function sendEmail(userNo) {
+  return request.get('/school/user/sendEmail', {
+    params: {
+      userNo: userNo
+    }
+  });
+}
+
+/**
+ * 验证邮件
+ * @param userNo
+ * @param code
+ */
+export function verifyEmail(userNo, code) {
+  return request.post('/school/user/verifyEmail', {
+    userNo,
+    code
+  });
 }
