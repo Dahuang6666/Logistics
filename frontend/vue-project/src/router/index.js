a585a6f006d3199797f10bd41ff2e64afb1688f6
huang yan <1514055091@qq.com>
2025-12-29 17:57:21 +0800

    宿舍管理系统-注册-登录-找回密码(重置版修改)

--- frontend/vue-project/src/router/index.js
+++ frontend/vue-project/src/router/index.js
@@ -1,19 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/views/LoginView.vue'
 import Register  from '@/views/Register.vue'
-import Test from '@/views/Test2.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'login',
-      component: LoginView,
+      component: LoginView
     },
     {
       path: '/register',
       name: 'register',
-      component: Register,
+      component: Register
     },
   ],
 })
